d8fb9bf9de217fc2fb7f3ebda64cf7d95c3bee6d
ryanmconstable <ryanmconstable@gmail.com>
2026-03-15 17:39:20 -0400

    Add simple file opener

--- Go/ToDo.go
+++ Go/ToDo.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"encoding/json"
+	"os"
 	"log"
 )
 
@@ -11,10 +11,11 @@ func main() {
 	file := IntakeParamaters()
 	fmt.Println("Opening ToDo List At:", file)
 
+	ParseFile(file)
 }
 
 func IntakeParamaters() string {
-	file := flag.String("file", "~/.todo", "Storage location for todo information")
+	file := flag.String("file", "todo.json", "Storage location for todo information")
 
 	flag.Parse()
 
@@ -25,4 +26,13 @@ func IntakeParamaters() string {
 
 	fmt.Println("END IntakeParameters\n")
 	return *file
+}
+
+func ParseFile(path string) {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
 }
