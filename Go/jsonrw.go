625f4a9aadd696356338254ad78b79bb839c8a06
ryanmconstable <ryanmconstable@gmail.com>
2026-03-29 13:03:28 -0400

    Refactored json to different file

---
+++ Go/jsonrw.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ParseFile(file string) Tasks {
+	jsonFile, err := os.Open(file)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	var tasks Tasks
+	err = json.Unmarshal(byteValue, &tasks)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+	}
+
+	return tasks
+}
+
+func WriteJson(tasks Tasks, file string) error {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(file, data, 0644)
+}
--- Go/main.go
+++ Go/main.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-)
+import ()
 
 type Tasks struct {
 	Tasks []Task `json:"tasks"`
@@ -53,34 +48,4 @@ func main() {
 	fields := []string{"Name", "Completed", "Date Created", "Date Completed", "Due Date"}
 
 	ShowTasks(tasks, fields)
-}
-
-func ParseFile(file string) Tasks {
-	jsonFile, err := os.Open(file)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-
-	byteValue, err := io.ReadAll(jsonFile)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-	}
-
-	var tasks Tasks
-	err = json.Unmarshal(byteValue, &tasks)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
-	}
-
-	return tasks
-}
-
-func WriteJson(tasks Tasks, file string) error {
-	data, err := json.Marshal(tasks)
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(file, data, 0644)
 }
