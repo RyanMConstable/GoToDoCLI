6275506714675861232faca658e0930e50a621ee
ryanmconstable <ryanmconstable@gmail.com>
2026-03-30 18:02:12 -0400

    Moved structs to separate file

--- Go/main.go
+++ Go/main.go
@@ -1,32 +1,5 @@
 package main
 
-import ()
-
-type Tasks struct {
-	Tasks []Task `json:"tasks"`
-}
-
-type Task struct {
-	Name          string `json:"name"`
-	Completed     bool   `json:"completed"`
-	DateCreated   string `json:"datecreated"`
-	DateCompleted string `json:"datecompleted"`
-	DueDate       string `json:"duedate"`
-}
-
-type Flags struct {
-	add       bool
-	duedate   bool
-	completed bool
-	remove    bool
-	all       bool
-}
-
-type FlagValues struct {
-	name    string
-	duedate string
-}
-
 func main() {
 	file := Setup()
 
