368289f7cf4abdc87dea5a14d5fae37b78b20823
ryanmconstable <ryanmconstable@gmail.com>
2026-04-15 19:20:40 -0400

    Cut down the setup file due to creation of .deb file

--- Go/setup.go
+++ Go/setup.go
@@ -1,33 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
-
 func Setup(d *Data) {
-	homeDirectory, _ := os.UserHomeDir()
-	defaultWorkingDirectory := homeDirectory + "/.todo"
-	defaultJsonFile := homeDirectory + "/.todo/todo.json"
-	defaultConfigFile := homeDirectory + "/.todo/config.yaml"
-
-	if _, err := os.Stat(defaultWorkingDirectory); os.IsNotExist(err) {
-		fmt.Println("Creating directory!")
-		os.Mkdir(defaultWorkingDirectory, 0o744)
-	}
-
-	if _, err := os.Stat(defaultJsonFile); err != nil {
-		data := []byte(`{"tasks": []}`)
-		err = os.WriteFile(defaultJsonFile, data, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	if _, err := os.Stat(defaultConfigFile); err != nil {
-		d.setup.confFile = ""
-	}
-
-	d.setup.dataFile = defaultJsonFile
+	d.setup.dataFile = "/etc/todo/todo.json"
+	d.setup.confFile = "/etc/todo/todo.conf"
 }
